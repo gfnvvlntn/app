9cfba0d7b5d8ab169b621a4f38b0195bf13df913
gfnvvlntn <gfnvvlntn@gmail.com>
2022-05-23 17:11:24 +0300

    customized date picker

--- client/src/components/base/date-picker/DatePicker.js
+++ client/src/components/base/date-picker/DatePicker.js
@@ -1,11 +1,12 @@
-import React, {forwardRef, useEffect, useState} from "react";
-import styled, {css} from "styled-components";
-import DatePicker, {registerLocale} from "react-datepicker";
+import React, { forwardRef, useEffect, useState } from "react";
+import styled, { css } from "styled-components";
+import DatePicker, { registerLocale } from "react-datepicker";
 import ru from "date-fns/locale/ru";
+import "theme/date-picker.css";
 
 registerLocale("ru", ru);
 
-const DateInput = ({onChange, value}) => {
+const DateInput = ({ onChange, value }) => {
   const [startDate, setStartDate] = useState(new Date());
 
   const handleChange = (date) => {
@@ -14,24 +15,24 @@ const DateInput = ({onChange, value}) => {
   };
 
   useEffect(() => {
-    onChange(startDate)
-  }, [])
+    onChange(startDate);
+  }, []);
 
-  const ExampleCustomInput = forwardRef(({value, onClick}, ref) => (
-      <Container onClick={onClick} ref={ref}>
-        {value}
-      </Container>
+  const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
+    <Container onClick={onClick} ref={ref}>
+      {value}
+    </Container>
   ));
   return (
-      <DatePicker
-          locale={ru}
-          dateFormat="dd.MM.yyyy p"
-          timeFormat="HH:mm"
-          selected={value ? value : startDate}
-          onChange={handleChange}
-          customInput={<ExampleCustomInput/>}
-          showTimeInput
-      />
+    <DatePicker
+      locale={ru}
+      dateFormat="dd.MM.yyyy p"
+      timeFormat="HH:mm"
+      selected={value ? value : startDate}
+      onChange={handleChange}
+      customInput={<ExampleCustomInput />}
+      showTimeSelect
+    />
   );
 };
 
@@ -40,14 +41,14 @@ DateInput.displayName = "DateInput";
 export default DateInput;
 
 const Container = styled("div")(
-    ({theme, error}) => css`
-      width: 100%;
-      border: 1px solid ${error ? theme.color.red : theme.color.four};
-      display: flex;
-      padding: 9.5px 14px;
-      border-radius: 0.428571rem;
-      background-color: ${theme.color.four};
-      box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-      cursor: pointer;
-    `
+  ({ theme, error }) => css`
+    width: 100%;
+    border: 1px solid ${error ? theme.color.red : theme.color.four};
+    display: flex;
+    padding: 9.5px 14px;
+    border-radius: 0.428571rem;
+    background-color: ${theme.color.four};
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    cursor: pointer;
+  `
 );
